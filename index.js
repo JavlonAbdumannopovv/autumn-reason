932240bccb9134b4bb975b786d6e9ecb9452f4d5
JavlonAbdumannopovv <javlonabdumannopovv@gmail.com>
2024-09-05 21:02:32 +0500

    added main section

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const app = document.querySelector('#app')
 const header = document.querySelector('header')
 const section = document.querySelector('#sidebar')
+const main = document.querySelector('main')
